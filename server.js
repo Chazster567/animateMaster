8853753337598649fce2692e2ffac8f43ea9b8be
Chazster567 <charles.whitehair@students.plymouth.ac.uk>
2020-04-21 17:37:18 +0100

    Moved to handlebars. Got mongo working. Tidied Up.

--- server.js
+++ server.js
@@ -1,13 +1,72 @@
 var express = require('express');
 var app = express();
-var texts = 'hello';
+var handlebars = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Contact = require('./models/Contact');
+var User = require('./models/User');
 
-for(i = 0; i < 10; i++) {
-    texts.length
-}
+app.set('view engine', 'hbs');
 
-app.get('/', (req, res)=>{
-    res.send(texts)
+app.engine('hbs', handlebars({
+    layoutDir: __dirname + '/views/layouts',
+    extname: 'hbs'
+}))
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render('login', { layout: 'main' });
+})
+
+app.get('/dashboard', (req, res) => {
+    Contact.find({}).lean()
+    .exec((err, contacts) => {
+        if(contacts.length){
+            res.render('dashboard', { layout: 'main', contacts: contacts, contactsExist: true });
+        } else{
+            res.render('dashboard', { layout: 'main', contacts: contacts, contactsExist: false });
+        }
+    })
+});
+
+app.post('/addContact', (req, res) =>{
+    const { name, email, number } = req.body;
+    var contact = new Contact({
+        name,
+        email,
+        number
+    });
+    contact.save();
+    res.redirect('/');
+})  
+
+app.post('/signup', (req, res) =>{
+    const { username, password } = req.body;
+    var user = new User({
+        username,
+        password
+    });
+    user.save();
+    res.redirect('/');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login', { layout: 'main' });
+})
+
+mongoose.connect('mongodb://localhost:27017/animate', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then(() => {
+    console.log('Connected to the DB');
+})
+.catch((err) => {
+    console.log('Not connected to the DB : ' + err);
 })
 
 //listening for requests on port 3000
