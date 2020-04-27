ade03fada06bf4f8de3937a9fccfd56f52ef60c8
Chazster567 <charles.whitehair@students.plymouth.ac.uk>
2020-04-27 22:24:33 +0100

    Fixed login feature. Now redirects and logs out.

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars');
 var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var session = require('express-session');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var { isAuth } = require('./middleware/isAuth')
 var User = require('./models/User');
 var Pet = require('./models/Pet')
+require('./middleware/passport')(passport);
 
 app.set('view engine', 'hbs');
 
@@ -15,6 +19,14 @@ app.engine('hbs', handlebars({
 }))
 
 app.use(express.static('public'));
+app.use(session({
+    secret: 'mySecret',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,6 +41,10 @@ app.get('/create', (req, res) => {
 
 app.get('/', (req, res) => {
     res.render('home', { layout: 'main' });
+})
+
+app.get('/home', isAuth, (req, res) => {
+    res.render('home', { layout: 'main', userLogged: true, username: req.user.username });
 })
 
 app.post('/signup', async (req, res) =>{
@@ -56,20 +72,21 @@ app.post('/signup', async (req, res) =>{
     }
 })
 
-app.post('/signin', async (req, res) =>{
-    const { username } = req.body;
+app.post('/signin', (req, res, next) =>{
     try{
-        let user = await User.findOne({ username });
-
-        if (user) {
-            return res.status(200).render('home', { layout: 'main', userExist: true, userName: user.username });
-        }
-
-        res.status(400).render('signIn', { layout: 'account', userDoesNotExist: true });
+        passport.authenticate('local', {
+            successRedirect: '/home',
+            failureRedirect: 'login'
+        })(req, res, next);
     } catch (err){
         console.log(err.message);
-        res.status(500).send('Server Error');
+        res.status(500).send('Server Error')
     }
+})
+
+app.get('/signout', (req, res) =>{
+    req.logOut();
+    res.redirect('/')
 })
 
 app.post('/petadd', (req, res) =>{
